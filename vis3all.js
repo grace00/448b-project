28f18c53b96de9dadbd3bfaf00323879c3988d9d
catzhang <cathy2zhang@gmail.com>
2023-03-24 19:11:37 -0700

    fix some things

--- vis3all.js
+++ vis3all.js
@@ -32,7 +32,7 @@
 
     // BOXPLOT 1 - AGE WHEN MET
     // set the dimensions and margins of the graph
-    var marginBox = {top: 10, right: 30, bottom: 30, left: 40},
+    var marginBox = {top: 10, right: 30, bottom: 30, left: 60},
     widthBox = 400 - marginBox.left - marginBox.right,
     heightBox = 400 - marginBox.top - marginBox.bottom;
 
@@ -142,74 +142,82 @@
                             .text("Aw, we don't have enough data for this! We'll work on it...");
                         return;
                     }
+                    // if (foundPermutation[1]["Hispanic"] !== '0' && foundPermutation[1]["Non-Hispanic"] !== '0') {
+                        // PIE CHART 1 - HISPANIC
+                        // set the color scale
+                        const color = d3.scaleOrdinal()
+                        .domain(["Hispanic", "Non-Hispanic"])
+                        .range(d3.schemeDark2);
 
-                    // PIE CHART 1 - HISPANIC
-                    // set the color scale
-                    const color = d3.scaleOrdinal()
-                    .domain(["Hispanic", "Non-Hispanic"])
-                    .range(d3.schemeDark2);
+                        // Compute the position of each group on the pie:
+                        const pie = d3.pie()
+                        .sort(null) // Do not sort group by size
+                        .value(d => d[1])
+                        const data_ready = pie(Object.entries(foundPermutation[1]))
 
-                    // Compute the position of each group on the pie:
-                    const pie = d3.pie()
-                    .sort(null) // Do not sort group by size
-                    .value(d => d[1])
-                    const data_ready = pie(Object.entries(foundPermutation[1]))
+                        // The arc generator
+                        const arc = d3.arc()
+                        .innerRadius(radius * 0.5)         // This is the size of the donut hole
+                        .outerRadius(radius * 0.8)
 
-                    // The arc generator
-                    const arc = d3.arc()
-                    .innerRadius(radius * 0.5)         // This is the size of the donut hole
-                    .outerRadius(radius * 0.8)
+                        // Another arc that won't be drawn. Just for labels positioning
+                        const outerArc = d3.arc()
+                        .innerRadius(radius * 0.9)
+                        .outerRadius(radius * 0.9)
 
-                    // Another arc that won't be drawn. Just for labels positioning
-                    const outerArc = d3.arc()
-                    .innerRadius(radius * 0.9)
-                    .outerRadius(radius * 0.9)
+                        // Build the pie chart: Basically, each part of the pie is a path that we build using the arc function.
+                        svgHispanic
+                        .selectAll('allSlices')
+                        .data(data_ready)
+                        .join('path')
+                        .attr('d', arc)
+                        .attr('fill', d => color(d.data[1]))
+                        .attr("stroke", "white")
+                        .style("stroke-width", "2px")
+                        .style("opacity", 0.7)
 
-                    // Build the pie chart: Basically, each part of the pie is a path that we build using the arc function.
-                    svgHispanic
-                    .selectAll('allSlices')
-                    .data(data_ready)
-                    .join('path')
-                    .attr('d', arc)
-                    .attr('fill', d => color(d.data[1]))
-                    .attr("stroke", "white")
-                    .style("stroke-width", "2px")
-                    .style("opacity", 0.7)
+                        // Add the polylines between chart and labels:
+                        svgHispanic
+                        .selectAll('allPolylines')
+                        .data(data_ready)
+                        .join('polyline')
+                            .attr("stroke", "black")
+                            .style("fill", "none")
+                            .attr("stroke-width", 1)
+                            .attr('points', function(d) {
+                            const posA = arc.centroid(d) // line insertion in the slice
+                            const posB = outerArc.centroid(d) // line break: we use the other arc generator that has been built only for that
+                            const posC = outerArc.centroid(d); // Label position = almost the same as posB
+                            const midangle = d.startAngle + (d.endAngle - d.startAngle) / 2 // we need the angle to see if the X position will be at the extreme right or extreme left
+                            posC[0] = radius * 0.95 * (midangle < Math.PI ? 1 : -1); // multiply by 1 or -1 to put it on the right or on the left
+                            return [posA, posB, posC]
+                            })
 
-                    // Add the polylines between chart and labels:
-                    svgHispanic
-                    .selectAll('allPolylines')
-                    .data(data_ready)
-                    .join('polyline')
-                        .attr("stroke", "black")
-                        .style("fill", "none")
-                        .attr("stroke-width", 1)
-                        .attr('points', function(d) {
-                        const posA = arc.centroid(d) // line insertion in the slice
-                        const posB = outerArc.centroid(d) // line break: we use the other arc generator that has been built only for that
-                        const posC = outerArc.centroid(d); // Label position = almost the same as posB
-                        const midangle = d.startAngle + (d.endAngle - d.startAngle) / 2 // we need the angle to see if the X position will be at the extreme right or extreme left
-                        posC[0] = radius * 0.95 * (midangle < Math.PI ? 1 : -1); // multiply by 1 or -1 to put it on the right or on the left
-                        return [posA, posB, posC]
-                        })
-
-                    // Add the polylines between chart and labels:
-                    svgHispanic
-                    .selectAll('allLabels')
-                    .data(data_ready)
-                    .join('text')
-                        .text(d => d.data[0])
-                        .attr('transform', function(d) {
-                            const pos = outerArc.centroid(d);
-                            const midangle = d.startAngle + (d.endAngle - d.startAngle) / 2
-                            pos[0] = radius * 0.99 * (midangle < Math.PI ? 1 : -1);
-                            return `translate(${pos})`;
-                        })
-                        .style('text-anchor', function(d) {
-                            const midangle = d.startAngle + (d.endAngle - d.startAngle) / 2
-                            return (midangle < Math.PI ? 'start' : 'end')
-                        })
-                        .style("font-size", "12px")
+                        // Add the polylines between chart and labels:
+                        svgHispanic
+                        .selectAll('allLabels')
+                        .data(data_ready)
+                        .join('text')
+                            .text(d => d.data[0])
+                            .attr('transform', function(d) {
+                                const pos = outerArc.centroid(d);
+                                const midangle = d.startAngle + (d.endAngle - d.startAngle) / 2
+                                pos[0] = radius * 0.99 * (midangle < Math.PI ? 1 : -1);
+                                return `translate(${pos})`;
+                            })
+                            .style('text-anchor', function(d) {
+                                const midangle = d.startAngle + (d.endAngle - d.startAngle) / 2
+                                return (midangle < Math.PI ? 'start' : 'end')
+                            })
+                            .style("font-size", "12px")
+                    // } else {
+                    //     svgHispanic.append("text")
+                    //         .attr("x", 10)             
+                    //         .attr("y", margin3.top)
+                    //         .attr("text-anchor", "middle")  
+                    //         .style("font-size", "16px") 
+                    //         .text("Aw, we don't have enough data!");
+                    // }
 
                     // PIE CHART 2 - RACE
 
@@ -255,12 +263,16 @@
                         .style("fill", "none")
                         .attr("stroke-width", 1)
                         .attr('points', function(d) {
-                        const posA = arcRace.centroid(d) // line insertion in the slice
-                        const posB = outerArcRace.centroid(d) // line break: we use the other arc generator that has been built only for that
-                        const posC = outerArcRace.centroid(d); // Label position = almost the same as posB
-                        const midangle = d.startAngle + (d.endAngle - d.startAngle) / 2 // we need the angle to see if the X position will be at the extreme right or extreme left
-                        posC[0] = radius * 0.95 * (midangle < Math.PI ? 1 : -1); // multiply by 1 or -1 to put it on the right or on the left
-                        return [posA, posB, posC]
+                            if (d.data[1] !== '0') {
+                                const posA = arcRace.centroid(d) // line insertion in the slice
+                                const posB = outerArcRace.centroid(d) // line break: we use the other arc generator that has been built only for that
+                                const posC = outerArcRace.centroid(d); // Label position = almost the same as posB
+                                const midangle = d.startAngle + (d.endAngle - d.startAngle) / 2 // we need the angle to see if the X position will be at the extreme right or extreme left
+                                posC[0] = radius * 0.95 * (midangle < Math.PI ? 1 : -1); // multiply by 1 or -1 to put it on the right or on the left
+                                return [posA, posB, posC]
+                            } else {
+                                return [0, 0, 0]
+                            }
                         })
 
                     // Add the polylines between chart and labels:
@@ -268,7 +280,11 @@
                     .selectAll('allLabelsRace')
                     .data(data_readyRace)
                     .join('text')
-                        .text(d => d.data[0])
+                        .text(d => {
+                            // return d.data[0];
+                            if (d.data[1] !== '0') { return d.data[0]; }
+                            else return '';
+                            })
                         .attr('transform', function(d) {
                             const pos = outerArcRace.centroid(d);
                             const midangle = d.startAngle + (d.endAngle - d.startAngle) / 2
@@ -377,7 +393,7 @@
                     .attr("y1", function(d){ return(y(d))} )
                     .attr("y2", function(d){ return(y(d))} )
                     .attr("stroke", "black")
-                    }
+                }
             );
         }
     );
